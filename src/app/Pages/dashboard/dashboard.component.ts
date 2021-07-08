7eb4b170b978923d35b782902b6562e2782c9dc5
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-07-08 12:59:34 +0530

    [Himanshu]:Added dashbord UI

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  userName = "Himanshu";
+  isMenuOpen = false;
+  contentMargin = 240
+  data: any
+  arr: any
+  length: any
+  // trashClick = false
+  // notesclick = false
+  // archiveClick = false
+  hide = true;
+  errors:any;
 
   constructor() { }
-
-  ngOnInit(): void {
+  onToolBarToggle() {
+    this.isMenuOpen = !this.isMenuOpen
+    if (!this.isMenuOpen) {
+      this.contentMargin = 50;
+    } else {
+      this.contentMargin = 240;
+    }
   }
 
-}
+  ngOnInit(): void {}
+  submit() {
+    // this.dataservice.getNotes().subscribe(res => {
+    //   this.data = res
+    //   this.arr = this.data.data
+    //   this.arr = this.arr.reverse()
+    //   this.length = this.arr.length
+    //   return this.length
+
+    // })
+  }
+   onTrashClick() {
+  //   this.trashClick = !this.trashClick
+  //   if (this.trashClick == true) {
+  //     this.route.navigate(['dashboard/trash']);
+  //     return this.trashClick = false
+  //   }
+  }
+
+  onNotesClick() {
+    // this.notesclick = !this.notesclick
+    // if (this.notesclick == true) {
+    //   this.route.navigate(['dashboard/notes']);
+    //   return this.notesclick = false
+    // }
+  }
+    onArchiveClick() {
+      // this.archiveClick = !this.archiveClick
+      // if (this.archiveClick == true) {
+      //   this.route.navigate(['dashboard/archive']);
+      //   return this.archiveClick = false
+      }
+    }
+
+
